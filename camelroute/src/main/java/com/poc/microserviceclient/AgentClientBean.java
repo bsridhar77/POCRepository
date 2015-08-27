182fcf7c772b299c0eded67b7998d62a5884c41f
386826 <386826@PC249234.cts.com>
2015-08-27 15:41:50 +0530

    Changes for update add agent

--- camelroute/src/main/java/com/poc/microserviceclient/AgentClientBean.java
+++ camelroute/src/main/java/com/poc/microserviceclient/AgentClientBean.java
@@ -1,6 +1,9 @@
 package com.poc.microserviceclient;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -15,9 +18,19 @@ import com.sun.jersey.api.client.WebResource;
 
 public class AgentClientBean {
 
+	
+      
+	public final static Map<String, Agent> agents = new TreeMap<String, Agent>();
+	 
 	Client client = Client.create();
 	public static String AGENT_URL="http://localhost:9080/agent";
 	
+	
+	AgentClientBean(){
+		agents.put("123", new Agent("1", "John Doe"));
+		agents.put("456", new Agent("2", "Donald Duck"));
+		agents.put("789", new Agent("3", "Slow Turtle"));
+	}
 	
 	public Agent getAgentDetails(String agentId) throws JsonParseException, JsonMappingException, IOException{
 		WebResource webResource = client.resource(AGENT_URL + "?agentId="+agentId);
@@ -35,8 +48,22 @@ public class AgentClientBean {
 		return agent;
 	}
 	
+	
+	  public void updateAgent(Agent agent) {
+		  System.out.println("**********updateAgent: Received Agent Id:"   + agent.getAgentId());
+		  System.out.println("**********updateAgent: Received Agent Name:" + agent.getAgentName());
+	   }
+	
+	  public Collection<Agent> listAgents() {
+		  
+	        return agents.values();
+	    }
+
+	  
+	  
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 		AgentClientBean agentClientBean = new AgentClientBean();
-		agentClientBean.getAgentDetails("3");
+		//agentClientBean.getAgentDetails("3");
+		//System.out.println(agentClientBean.listAgents().toString());
 	}
 }
--- camelroute/src/main/java/com/poc/routes/POCRouteBuilder.java
+++ camelroute/src/main/java/com/poc/routes/POCRouteBuilder.java
@@ -1,19 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.poc.routes;
 
 
@@ -25,31 +10,28 @@ import com.poc.model.Agent;
 /**
  * Define REST services using the Camel REST DSL
  */
+
 public class POCRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-
-        // configure we want to use servlet as the component for the rest DSL
-        // and we enable json binding mode
-    	
         restConfiguration().component("servlet").bindingMode(RestBindingMode.json)
-            // and output using pretty print
+
             .dataFormatProperty("prettyPrint", "true")
             .contextPath("camelroute/rest").port(8080);
 
-        // this user REST service is json only
-        rest("/agent").description("agent rest service")
+
+        rest("/agent").description("Agent Rest Service")
             .consumes("application/json").produces("application/json")
 
             .get("/{id}").description("Find agent by id").outType(Agent.class)
-                .to("bean:agentClientBean?method=getAgentDetails(${header.id})");
-        /*    
+                .to("bean:agentClientBean?method=getAgentDetails(${header.id})")
+            
             .put().description("Updates or create a Agent").type(Agent.class)
                 .to("bean:agentClientBean?method=updateAgent")
 
             .get("/findAll").description("Find all Agents").outTypeList(Agent.class)
-                .to("bean:agentClientBean?method=listAgents");*/
+                .to("bean:agentClientBean?method=listAgents");
     }
 
 }
